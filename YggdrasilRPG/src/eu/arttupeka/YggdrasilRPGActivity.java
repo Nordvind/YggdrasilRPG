a864b61fa595216640854689dcce444688944c0c
Nordvind <APeka1990@gmail.com>
2012-01-30 00:09:54 +0200

    init

---
+++ YggdrasilRPG/src/eu/arttupeka/YggdrasilRPGActivity.java
@@ -0,0 +1,13 @@
+package eu.arttupeka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YggdrasilRPGActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
